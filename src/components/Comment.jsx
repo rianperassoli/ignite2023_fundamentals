0815774585b1b7dc68de5caa320c4fe929a94e39
Rian Perassoli <rian.perassoli@gmail.com>
2023-09-24 14:09:18 -0300

    adicionada funcao de aplaudir post

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -33,8 +40,8 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
-            <ThumbsUp /> Aplaudir <span>20</span>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp /> Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
